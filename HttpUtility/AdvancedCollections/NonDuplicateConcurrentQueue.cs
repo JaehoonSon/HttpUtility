fb4651ba597e272f275f62301d43d51c9c5ef175
Knuceles <son.jaehoon0826@gmail.com>
2023-09-27 19:03:21 -0400

    Add project files.

---
+++ HttpUtility/AdvancedCollections/NonDuplicateConcurrentQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace HttpUtility;
+
+public class NonDuplicateConcurrentQueue<T> : IDisposable
+{
+    private HashSet<T> set = new();
+    public void AddToQueue(ref ConcurrentQueue<T> que, T item)
+    {
+        if (!set.Contains(item))
+        {
+            que.Enqueue(item);
+            set.Add(item);
+        }
+    }
+
+    //Only use this method if you want the class to keep track of unique items in the set; otherwise, do not use this method
+    public bool RemoveFromQueue(ref ConcurrentQueue<T> que, out T item)
+    {
+        if (que.TryDequeue(out var item2))
+        {
+            item = item2;
+            set.Remove(item2);
+            return true;
+        }
+
+        item = item2;
+        return false;
+    }
+    public void Dispose()
+    { }
+}
---
+++ HttpUtility/UniversalClient.cs
@@ -0,0 +1,61 @@
+﻿namespace HttpUtility;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpUtility;
+
+public class UniversalClient : IDisposable
+{
+    public static int _success = 0;
+    private static bool _isRunning = true;
+    private bool _isDisposed = false;
+    private readonly HttpClient _client;
+
+    public static string? BaseURL { get; set; }
+
+    public UniversalClient(WebProxy? proxy = null)
+    {
+        HttpClientHandler handler = new()
+        {
+            Proxy = proxy,
+            UseProxy = proxy is not null,
+        };
+
+
+        _client = new(handler);
+        ClientHelper.AddRandomUserAgent(ref _client);
+    }
+
+
+    public async Task GetHttpByNumber(int number)
+    {
+        switch (number)
+        {
+            case -1:
+                while (_isRunning)
+                {
+                    await _client.GetAsync(BaseURL);
+                    Interlocked.Increment(ref _success);
+                }
+                break;
+            case > 0:
+                for (int i = 0; i < number; i++)
+                {
+                    await _client.GetAsync(BaseURL);
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+
+    public void Dispose()
+    {
+    }
+}

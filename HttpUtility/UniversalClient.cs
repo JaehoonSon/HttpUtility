633f448b349876ffd30ee12f431d5f65215e5221
knuceles <son.jaehoon0826@gmail.com>
2023-10-12 23:25:03 -0400

    Added query support and http reads

--- HttpUtility/UniversalClient.cs
+++ HttpUtility/UniversalClient.cs
@@ -6,7 +6,6 @@ public class UniversalClient : IDisposable
 {
     public static int _success = 0;
     private static bool _isRunning = true;
-    private bool _isDisposed = false;
     private readonly HttpClient _client;
 
     public static string? BaseURL { get; set; }
@@ -24,28 +23,67 @@ public class UniversalClient : IDisposable
         ClientHelper.AddRandomUserAgent(ref _client);
     }
 
-
-    public async Task GetHttpByNumber(int number)
+    public async Task ReadHttp()
     {
-        switch (number)
+        try
         {
-            case -1:
-                while (_isRunning)
-                {
-                    await _client.GetAsync(BaseURL);
-                    Interlocked.Increment(ref _success);
-                }
-                break;
-            case > 0:
-                for (int i = 0; i < number; i++)
-                {
-                    await _client.GetAsync(BaseURL);
-                }
-                break;
-            default:
-                Console.WriteLine("Invalid");
-                break;
+            var data = await _client.GetAsync(BaseURL);
+            Console.WriteLine(await data.Content.ReadAsStringAsync());
         }
+        catch { }
+    }
+
+    public static void AddQuery(string key, string value)
+    {
+        string query = "?";
+        var values = value.Split(" ");
+        if (values.Length == 0)
+        {
+            query = "";
+            Console.WriteLine("Invalid value");
+        }
+        else if (values.Length == 1)
+        {
+            query += key + "=" + values[0];
+        }
+        else
+        {
+            query += key + "=" + values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                query += "+" + values[i];
+            }
+        }
+        BaseURL += query;
+    }
+
+
+    public async Task GetHttpByNumberAsync(int number)
+    {
+        try
+        {
+            switch (number)
+            {
+                case -1:
+                    while (_isRunning)
+                    {
+                        await _client.GetAsync(BaseURL);
+                        Interlocked.Increment(ref _success);
+                    }
+                    break;
+                case > 0:
+                    for (int i = 0; i < number; i++)
+                    {
+                        await _client.GetAsync(BaseURL);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        }
+        catch { }
+
     }
 
     public void Dispose()
